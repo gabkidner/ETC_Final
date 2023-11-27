cb5a735cc4756046b7e2d1ba8e66d145d61c041a
gabkidner <gkidner07@gmail.com>
2023-11-27 08:38:09 -0700

    Completed the Assignment

--- python/student_code/engine.py
+++ python/student_code/engine.py
@@ -17,7 +17,8 @@ def decide(scenario):
         "pedestrians". Note that your method MUST make a decision for
         ANY potential scenario.
     """
-
+    pas = 0
+    ped = 0
     # NOTE: YOU NEED TO REPLACE ALL OF THE CODE BELOW!!!
     # This simply demonstrates how to access information from the scenario.
     # print("Are the pedestrians crossing legally?", scenario.legalCrossing)
@@ -32,9 +33,37 @@ def decide(scenario):
     # Your program must choose to save either pedestrians or passengers.
     # This is an overly simple rule that only saves the passengers if there are
     # more passengers than pedestrians.
-
-    if len(scenario.passengers) > len(scenario.pedestrians):
-        return "passengers"
-    else:
+    for Person in scenario.pedestrians:
+        if(Person.charType != "animal"):
+            ped += 1
+        if(Person.age == "child" or Person.age == "baby"):
+            ped += 3
+        if(Person.age == "adult"):
+            ped += 2
+        if(Person.age == "elderly"):
+            ped += 1
+        if(Person.profession == "doctor"):
+            ped += 1
+        if(Person.profession == "criminal"):
+            ped -= 1
+        if(Person.pregnant):
+            ped += 1
+    for Person in scenario.passengers:
+        if(Person.charType != "animal"):
+            pas += 1
+        if(Person.age == "child" or Person.age == "baby"):
+            pas += 3
+        if(Person.age == "adult"):
+            pas += 2
+        if(Person.age == "elderly"):
+            pas += 1
+        if(Person.profession == "doctor"):
+            pas += 1
+        if(Person.profession == "criminal"):
+            pas -= 1
+        if(Person.pregnant):
+            pas += 1
+    if ped > pas:
         return "pedestrians"
-    
+    else:
+        return "passengers"
